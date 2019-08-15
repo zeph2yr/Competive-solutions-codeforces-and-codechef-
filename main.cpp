4e492d5d4253e05d396b99140bc86260dae56c0a
zeph2yr <42335893+zeph2yr@users.noreply.github.com>
2019-08-15 22:35:04 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,13 +1,32 @@
-#include <iostream>
+#include<iostream>
+#include<string>
 
 using namespace std;
 
 int main()
 {
-    int x11, x12, x13, x21, x22, x23, x31, x32, x33;
-    cin >> x11 >> x12 >> x13 >> x21 >> x22 >> x23 >> x31 >> x32 >> x33;
-    cout << ((x11 + x12 + x21) % 2 == 0 ? "1" : "0") << ((x11 + x12 + x13 + x22) % 2 == 0 ? "1" : "0") << ((x12 + x13 + x23) % 2 == 0 ? "1" : "0") << endl;
-    cout << ((x11 + x21 + x22 + x31) % 2 == 0 ? "1" : "0") << ((x12 + x21 + x22 + x23 + x32) % 2 == 0 ? "1" : "0") << ((x13 + x22 + x23 + x33) % 2 == 0 ? "1" : "0") << endl;
-    cout << ((x21 + x31 + x32) % 2 == 0 ? "1" : "0") << ((x22 + x31 + x32 + x33) % 2 == 0 ? "1" : "0") << ((x23 + x32 + x33) % 2 == 0 ? "1" : "0") << endl;
-    return 0;
+  string x;
+  int n,v1=0,v2=0,cnt=0;
+  cin>>n;
+  for(int i=0;i<n;i++)
+  {
+      cin>>x;
+      if(x==0)
+      {
+          v1++;
+      }
+      else if(x==1)
+      {
+          v2++;
+      }
+
+      while(v1!=v2)
+      {
+         cnt++;
+      }
+  }
+   cout<<cnt<<"\n";
+
+
+
 }
