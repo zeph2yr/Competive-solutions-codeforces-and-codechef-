c562f1f653642b930f69245622196d270452bef6
Aryaman Tyagi <42335893+zeph2yr@users.noreply.github.com>
2020-11-04 22:36:05 +0530

    Add files via upload

---
+++ redornot.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    string s;
+    cin>>a;
+    cin>>s;
+    if(a>=3200)
+    {
+        cout<<s;
+    }
+    else cout<<"red";
+}
